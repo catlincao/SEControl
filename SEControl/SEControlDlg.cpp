021f4b83c9eff24e3f2612f04f1fdd8f3f9e2176
catlincao <catlincao@yeah.net>
2018-07-25 15:54:02 +0800

    SEControl

---
+++ SEControl/SEControlDlg.cpp
@@ -0,0 +1,188 @@
+
+// SEControlDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "SEControl.h"
+#include "SEControlDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CSEControlDlg 对话框
+
+
+
+CSEControlDlg::CSEControlDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_SECONTROL_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CSEControlDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_COMBO1, m_choseCAM);
+}
+
+BEGIN_MESSAGE_MAP(CSEControlDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_CBN_SELCHANGE(IDC_COMBO1, &CSEControlDlg::OnCbnSelchangeCombo1)
+	ON_BN_CLICKED(ID_OPEN, &CSEControlDlg::OnBnClickedOpen)
+END_MESSAGE_MAP()
+
+
+// CSEControlDlg 消息处理程序
+
+BOOL CSEControlDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+	ComListInit();
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CSEControlDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CSEControlDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CSEControlDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CSEControlDlg::ComListInit()
+{
+	m_choseCAM.AddString(L"CAM1");
+	m_choseCAM.AddString(L"CAM2");
+	m_choseCAM.AddString(L"CAM3");
+	m_choseCAM.AddString(L"CAM4");
+	m_choseCAM.AddString(L"CAM5");
+	m_choseCAM.AddString(L"CAM6");
+}
+
+void CSEControlDlg::OnCbnSelchangeCombo1()
+{
+	cam_index = m_choseCAM.GetCurSel();
+	switch (cam_index)
+	{
+	case 0: lpPara = L"openCamera1";		break;
+	case 1: lpPara = L"openCamera2";		break;
+	case 2: lpPara = L"openCamera3";		break;
+	case 3: lpPara = L"openCamera4";		break;
+	case 4: lpPara = L"openCamera5";		break;
+	case 5: lpPara = L"openCamera6";		break;
+	default: lpPara = L"openCamera1";		break;
+	}
+	
+}
+
+
+void CSEControlDlg::OnBnClickedOpen()
+{
+	ShellExecute(NULL, L"open", L"G:\\documents\\ImageSaver\\debug\\ImageSaver.exe", lpPara, NULL, SW_SHOWNORMAL);
+}
